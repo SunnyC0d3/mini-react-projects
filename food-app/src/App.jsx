19ce10d66e8a188ea1fe72d7987a53a1e1eb2c36
Sukhwinder <singhdevelops@gmail.com>
2024-07-16 12:07:05 +0100

    building food app

---
+++ food-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+import Cart from "./components/Cart";
+import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/UserProgressContext";
+
+function App() {
+  return (
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
+  );
+}
+
+export default App;
